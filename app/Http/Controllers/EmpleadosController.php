10b96a51786e2d255e32ffbe36b65936c463e8f9
Bonkis <aperez6636@misena.edu.co>
2021-03-03 14:23:39 -0500

    Insertando el proyecto

---
+++ app/Http/Controllers/EmpleadosController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmpleadosController extends Controller
+{
+    public function Index(){
+        return "este es el index del controlador empleados";
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,1 @@
+<h1>Mundo Laravel</h1>
